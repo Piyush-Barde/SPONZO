0cedb4d8c692621651e86ec5dce241a79a7aebf2
Piyush Barde <piyushbarde01@gmail.com>
2025-12-18 00:11:16 +0530

    New Script link updated again-4

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ async function submitForm(form, apiEndpoint, modalType) {
 ================================ */
 const organizerForm = document.getElementById("organizerForm");
 const ORGANIZER_API_URL =
-  "https://script.google.com/macros/s/AKfycby3VRu18k_AcKIZipIay_8Mg8lrJ6iDJWYLS1SwfTLdnD-b7ZWPIncU1K8Zq9NSGu5srw/exec";
+  "https://script.google.com/macros/s/AKfycbz-xKZjncZQIUH_Julj7rYrjqiDUHqVYwuaoacV55-xlatSKHcKm95jkloIM74TTDJaOw/exec";
 
 if (organizerForm) {
   organizerForm.addEventListener("submit", e => {
