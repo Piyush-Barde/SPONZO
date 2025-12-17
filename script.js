71191a5787594e52179269a148d053ca27e6217a
Piyush Barde <piyushbarde01@gmail.com>
2025-12-17 23:32:32 +0530

    Updated Event script

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ async function submitForm(form, apiEndpoint, modalType) {
 ================================ */
 const organizerForm = document.getElementById("organizerForm");
 const ORGANIZER_API_URL =
-  "PASTE_EVENT_ORGANIZER_SCRIPT_URL_HERE";
+  "https://script.google.com/macros/s/AKfycbysgiyhLNkRqL8QTUjSzaNiNRaIWjidsMWAfWIVtdr2SSHawuuWvPG4sQGh4cyY8PPK4Q/exec";
 
 if (organizerForm) {
   organizerForm.addEventListener("submit", e => {
